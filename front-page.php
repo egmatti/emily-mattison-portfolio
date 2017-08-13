9586148cf52e620d586705efc72fc5c1fee0c8c8
Emily Mattison <egmatti@gmail.com>
2017-08-13 15:07:39 -0400

    Set up the blog post loop on the front-page.php page

--- front-page.php
+++ front-page.php
@@ -29,223 +29,56 @@ FRONT PAGE
 
       <div id="blog-div" class="left-column">
         <div class="blog-slider-for">
-          <div class="blog-slider-for__post">
-            <figure class="share-figure">
-              <img src="images/the-latest/blog-posts/the-iron-yard.png" alt="Blog Post 7">
-              <figcaption>
-                <div class="share-container">
-                  <span>Share</span>
-                </div>
-                <div class="blog-social-media-container">
-                  <ul class="blog-social-media">
-                    <li class="blog-social-media__item"><a><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                    <li class="blog-social-media__item"><a><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
-                    <li class="blog-social-media__item"><a><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                  </ul>
-                </div>
-              </figcaption>
-            </figure>
-            <article class="blog-post-container">
-              <span>June 7, 2017</span>
-              <h3>Blog Post 7</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Donec elementum ligula eu sapien consequat eleifend. Donec nec dolor erat, condimentum sagittis sem.</p>
-            </article>
-          </div>
-          <div class="blog-slider-for__post">
-            <figure class="share-figure">
-              <img src="images/the-latest/blog-posts/stax.png" alt="Blog Post 6">
-              <figcaption>
-                <div class="share-container">
-                  <span>Share</span>
-                </div>
-                <div class="blog-social-media-container">
-                  <ul class="blog-social-media">
-                    <li class="blog-social-media__item"><a><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                    <li class="blog-social-media__item"><a><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
-                    <li class="blog-social-media__item"><a><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                  </ul>
-                </div>
-              </figcaption>
-            </figure>
-            <article class="blog-post-container">
-              <span>June 6, 2017</span>
-              <h3>Blog Post 6</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
-            </article>
-          </div>
-          <div class="blog-slider-for__post">
-            <figure class="share-figure">
-              <img  src="images/projects/coffee-app/mockups/coffeeapp_mockups_Voyage.png" alt="Blog Post 5">
-              <figcaption>
-                <div class="share-container">
-                  <span>Share</span>
-                </div>
-                <div class="blog-social-media-container">
-                  <ul class="blog-social-media">
-                    <li class="blog-social-media__item"><a><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                    <li class="blog-social-media__item"><a><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
-                    <li class="blog-social-media__item"><a><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                  </ul>
-                </div>
-              </figcaption>
-            </figure>
-            <article class="blog-post-container">
-              <span>June 5, 2017</span>
-              <h3>Blog Post 5</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
-            </article>
-          </div>
-          <div class="blog-slider-for__post">
-            <figure class="share-figure">
-              <img src="images/the-latest/blog-posts/rocket.png" alt="Blog Post 4">
-              <figcaption>
-                <div class="share-container">
-                  <span>Share</span>
-                </div>
-                <div class="blog-social-media-container">
-                  <ul class="blog-social-media">
-                    <li class="blog-social-media__item"><a><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                    <li class="blog-social-media__item"><a><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
-                    <li class="blog-social-media__item"><a><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                  </ul>
-                </div>
-              </figcaption>
-            </figure>
-            <article class="blog-post-container">
-              <span>June 4, 2017</span>
-              <h3>Blog Post 4</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
-            </article>
-          </div>
-          <div class="blog-slider-for__post">
-            <figure class="share-figure">
-              <img src="images/the-latest/blog-posts/a3c.png" alt="Blog Post 3">
-              <figcaption>
-                <div class="share-container">
-                  <span>Share</span>
-                </div>
-                <div class="blog-social-media-container">
-                  <ul class="blog-social-media">
-                    <li class="blog-social-media__item"><a><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                    <li class="blog-social-media__item"><a><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
-                    <li class="blog-social-media__item"><a><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                  </ul>
-                </div>
-              </figcaption>
-            </figure>
-            <article class="blog-post-container">
-              <span>June 3, 2017</span>
-              <h3>Blog Post 3</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.</p>
-            </article>
-          </div>
-          <div class="blog-slider-for__post">
-            <figure class="share-figure">
-              <img src="images/the-latest/blog-posts/pablo.png" alt="Blog Post 2">
-              <figcaption>
-                <div class="share-container">
-                  <span>Share</span>
-                </div>
-                <div class="blog-social-media-container">
-                  <ul class="blog-social-media">
-                    <li class="blog-social-media__item"><a><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                    <li class="blog-social-media__item"><a><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
-                    <li class="blog-social-media__item"><a><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                  </ul>
-                </div>
-              </figcaption>
-            </figure>
-            <article class="blog-post-container">
-              <span>June 2, 2017</span>
-              <h3>Blog Post 2</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.</p>
-            </article>
-          </div>
-          <div class="blog-slider-for__post">
-            <figure class="share-figure">
-              <img src="images/the-latest/blog-posts/sullivan's.png" alt="Blog Post 1">
-              <figcaption>
-                <div class="share-container">
-                  <span>Share</span>
-                </div>
-                <div class="blog-social-media-container">
-                  <ul class="blog-social-media">
-                    <li class="blog-social-media__item"><a><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                    <li class="blog-social-media__item"><a><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
-                    <li class="blog-social-media__item"><a><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                  </ul>
-                </div>
-              </figcaption>
-            </figure>
-            <article class="blog-post-container">
-              <span>June 1, 2017</span>
-              <h3>Blog Post 1</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</p>
-            </article>
-          </div>
+
+          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+            <div class="blog-slider-for__post">
+              <figure class="share-figure">
+                <img src="images/the-latest/blog-posts/the-iron-yard.png" alt="Blog Post 7">
+                <figcaption>
+                  <div class="share-container">
+                    <span>Share</span>
+                  </div>
+                  <div class="blog-social-media-container">
+                    <ul class="blog-social-media">
+                      <li class="blog-social-media__item"><a><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                      <li class="blog-social-media__item"><a><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+                      <li class="blog-social-media__item"><a><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    </ul>
+                  </div>
+                </figcaption>
+              </figure>
+              <article class="blog-post-container">
+                <span>June 7, 2017</span>
+                <h3><?php the_title(); ?></h3>
+                <p><?php the_content(); ?></p>
+              </article>
+            </div>
+
+          <?php endwhile; else : ?>
+            <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+          <?php endif; ?>
+
         </div>
       </div>
 
       <div id="blog-column" class="right-column">
         <div class="blog-slider-nav directory-nav">
-          <div class="blog-slider-nav__post directory-nav__post">
-            <img src="images/the-latest/blog-posts/the-iron-yard.png" alt="Blog Post 7">
-            <article>
-              <span>June 7, 2017</span>
-              <h5>Blog Post 7</h5>
-            </article>
-          </div>
-          <div class="blog-slider-nav__post directory-nav__post">
-            <img src="images/the-latest/blog-posts/stax.png" alt="Blog Post 6">
-            <article>
-              <span>June 6, 2017</span>
-              <h5>Blog Post 6</h5>
-            </article>
-          </div>
-          <div class="blog-slider-nav__post directory-nav__post">
-            <img src="images/projects/coffee-app/mockups/coffeeapp_mockups_Voyage.png" alt="Blog Post 5">
-            <article>
-              <span>June 5, 2017</span>
-              <h5>Blog Post 5</h5>
-            </article>
-          </div>
-          <div class="blog-slider-nav__post directory-nav__post">
-            <img src="images/the-latest/blog-posts/rocket.png" alt="Blog Post 4">
-            <article>
-              <span>June 4, 2017</span>
-              <h5>Blog Post 4</h5>
-            </article>
-          </div>
-          <div class="blog-slider-nav__post directory-nav__post">
-            <img src="images/the-latest/blog-posts/a3c.png" alt="Blog Post 3">
-            <article>
-              <span>June 3, 2017</span>
-              <h5>Blog Post 3</h5>
-            </article>
-          </div>
-          <div class="blog-slider-nav__post directory-nav__post">
-            <img src="images/the-latest/blog-posts/pablo.png" alt="Blog Post 2">
-            <article>
-              <span>June 2, 2017</span>
-              <h5>Blog Post 2</h5>
-            </article>
-          </div>
-          <div class="blog-slider-nav__post directory-nav__post">
-            <img src="images/the-latest/blog-posts/sullivan's.png" alt="Blog Post 1">
-            <article>
-              <span>June 1, 2017</span>
-              <h5>Blog Post 1</h5>
-            </article>
-          </div>
+
+          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+            <div class="blog-slider-nav__post directory-nav__post">
+              <img src="images/the-latest/blog-posts/the-iron-yard.png" alt="Blog Post 7">
+              <article>
+                <span>June 7, 2017</span>
+                <h5><?php the_title(); ?></h5>
+              </article>
+            </div>
+
+          <?php endwhile; else : ?>
+  	         <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+          <?php endif; ?>
+
         </div>
       </div>
     </section>
