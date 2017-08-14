065846188319b2d6c3e44e3f61c9aeaa2de5d114
Emily Mattison <egmatti@gmail.com>
2017-08-14 15:09:48 -0400

    Created the Latest page template and added the Latest page

---
+++ page-the-latest.php
@@ -0,0 +1,179 @@
+<!-- ==========================================
+THE LATEST PAGE
+========================================== -->
+
+<?php
+/*
+  Template Name: The Latest Page
+*/
+?>
+
+
+<?php get_header(); ?>
+
+
+<!-- HERO -->
+
+    <section class="hero-section">
+      <div class="headline-container">
+        <h1><span>the   </span>Latest</h1>
+      </div>
+    </section>
+
+
+<!-- CONTENT SECTION -->
+
+    <section class="content-section">
+
+      <div class="directory-nav-container">
+        <h4>Recent</h4>
+        <div class="directory-nav-border"></div>
+        <div class="directory-nav">
+          <a href="blog-post.html"><div class="directory-nav__post">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/the-latest/blog-posts/the-iron-yard.png" alt="Blog Post 7">
+            <article>
+              <span>June 7, 2017</span>
+              <h5>Blog Post 7</h5>
+            </article>
+          </div></a>
+          <a><div class="directory-nav__post">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/the-latest/blog-posts/stax.png" alt="Blog Post 6">
+            <article>
+              <span>June 6, 2017</span>
+              <h5>Blog Post 6</h5>
+            </article>
+          </div></a>
+          <a><div class="directory-nav__post">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/projects/coffee-app/mockups/coffeeapp_mockups_Voyage.png" alt="Blog Post 5">
+            <article>
+              <span>June 5, 2017</span>
+              <h5>Blog Post 5</h5>
+            </article>
+          </div></a>
+          <a><div class="directory-nav__post">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/the-latest/blog-posts/rocket.png" alt="Blog Post 4">
+            <article>
+              <span>June 4, 2017</span>
+              <h5>Blog Post 4</h5>
+            </article>
+          </div></a>
+          <a><div class="directory-nav__post">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/the-latest/blog-posts/a3c.png" alt="Blog Post 3">
+            <article>
+              <span>June 3, 2017</span>
+              <h5>Blog Post 3</h5>
+            </article>
+          </div></a>
+          <a><div class="directory-nav__post">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/the-latest/blog-posts/pablo.png" alt="Blog Post 2">
+            <article>
+              <span>June 2, 2017</span>
+              <h5>Blog Post 2</h5>
+            </article>
+          </div></a>
+          <a><div class="directory-nav__post">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/the-latest/blog-posts/sullivan's.png" alt="Blog Post 1">
+            <article>
+              <span>June 1, 2017</span>
+              <h5>Blog Post 1</h5>
+            </article>
+          </div></a>
+        </div>
+      </div>
+
+      <div class="directory">
+        <div class="directory__row--first">
+          <a href="blog-post.html" class="directory__link--first-row"><figure class="directory__figure--first-row">
+            <div class="push-pin-container">
+              <img class="push-pin" src="<?php bloginfo('stylesheet_directory'); ?>/images/the-latest/push-pin.png" alt="Push pin">
+            </div>
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/the-latest/blog-posts/the-iron-yard.png" alt="The Iron Yard classroom">
+            <figcaption>
+              <div>
+                <span>June 7, 2017</span>
+                <h2>Blog Post 7</h2>
+              </div>
+            </figcaption>
+          </figure></a>
+        </div>
+
+        <div class="directory__row--second">
+          <a class="directory__link--second-row"><figure class="directory__figure--second-row">
+            <div class="directory__image-container">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/the-latest/blog-posts/stax.png" alt="Jim Stewart and Estelle Axton from Stax Records">
+            </div>
+            <figcaption>
+              <div>
+                <span>June 6, 2017</span>
+                <h3>Blog Post 6</h3>
+              </div>
+            </figcaption>
+          </figure></a>
+          <a class="directory__link--second-row"><figure class="directory__figure--second-row">
+            <div class="directory__image-container">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/the-latest/blog-posts/coffee-app.png" alt="Voyage screen from my Coffee App">
+            </div>
+            <figcaption>
+              <div>
+                <span>June 5, 2017</span>
+                <h3>Blog Post 5</h3>
+              </div>
+            </figcaption>
+          </figure></a>
+        </div>
+
+        <div class="directory__row--other">
+          <a class="directory__link--other-row"><figure class="directory__figure--other-row">
+            <div class="directory__image-container">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/the-latest/blog-posts/rocket-gray-background.png" alt="Early rocket illustrations for Shane Daly">
+            </div>
+            <figcaption>
+              <div>
+                <span>June 4, 2017</span>
+                <h4>Blog Post 4</h4>
+              </div>
+            </figcaption>
+          </figure></a>
+          <a class="directory__link--other-row"><figure class="directory__figure--other-row">
+            <div class="directory__image-container">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/the-latest/blog-posts/a3c.png" alt="Hip Hop photography panel at the A3C Conference in October 2016">
+            </div>
+            <figcaption>
+              <div>
+                <span>June 3, 2017</span>
+                <h4>Blog Post 3</h4>
+              </div>
+            </figcaption>
+          </figure></a>
+          <a class="directory__link--other-row"><figure class="directory__figure--other-row">
+            <div class="directory__image-container">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/the-latest/blog-posts/pablo.png" alt="Kanye West performing in Atlanta as part of his 2016 Saint Pablo Tour">
+            </div>
+            <figcaption>
+              <div>
+                <span>June 2, 2017</span>
+                <h4>Blog Post 2</h4>
+              </div>
+            </figcaption>
+          </figure></a>
+        </div>
+
+        <div class="directory__row--last">
+          <a class="directory__link--other-row"><figure class="directory__figure--other-row">
+            <div class="directory__image-container">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/images/the-latest/blog-posts/sullivan's.png" alt="My golden retriever enjoying a great Saturday at Sullivan's Island">
+            </div>
+            <figcaption>
+              <div>
+                <span>June 1, 2017</span>
+                <h4>Blog Post 1</h4>
+              </div>
+            </figcaption>
+          </figure></a>
+
+      </div>
+
+    </section>
+
+
+<?php get_footer(); ?>
