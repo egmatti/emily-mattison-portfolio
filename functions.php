3bf2e494f09ab679d921c9115dede9564c14622d
Emily Mattison <egmatti@gmail.com>
2017-08-15 13:45:11 -0400

    Added 'p' tag remover plugin and function to remove width attributes from post images

--- functions.php
+++ functions.php
@@ -116,4 +116,15 @@ function the_post_thumbnail_alt($post_id) {
   echo get_the_post_thumbnail_alt($post_id);
 }
 
+
+// REMOVE WIDTH ATTRIBUTE FROM POST IMAGES
+
+add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 );
+add_filter( 'image_send_to_editor', 'remove_width_attribute', 10 );
+
+function remove_width_attribute( $html ) {
+   $html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
+   return $html;
+}
+
 ?>
