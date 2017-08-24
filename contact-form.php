7e9e2c3f3046bb14ea840683cedbaba7315c4b58
Emily Mattison <egmatti@gmail.com>
2017-08-23 22:34:07 -0400

    Added new media queries for contact form

--- contact-form.php
+++ contact-form.php
@@ -4,36 +4,6 @@ CONTACT FORM
 
 <section class="contact-section">
 
-  <!-- <div class="left-column"> -->
-    <?php echo FrmFormsController::get_form_shortcode( array( 'id' => 2, 'title' => false, 'description' => false ) ); ?>
-
-    <!-- <form class="contact-form">
-      <h3>Get In Touch</h3>
-      <div class="input-container">
-        <input class="input__contact" id="first-name" type="text" required>
-        <label for="first-name">First Name</label>
-      </div>
-      <div class="input-container">
-        <input class="input__contact" id="last-name" type="text" required>
-        <label for="last-name">Last Name</label>
-      </div>
-      <div class="input-container--last">
-        <input class="input__contact" id="email-address" type="text" required>
-        <label for="email-address">Email Address</label>
-      </div>
-      <div class="textarea-container">
-        <textarea class="input__contact--message" id="message" type="textarea" name="message" required></textarea>
-        <label for="message">Message</label>
-      </div>
-    </form> -->
-  <!-- </div>
-
-  <div class="right-column button-div">
-    <div class="button-container">
-      <div class="button button-send">
-        <span>Send</span>
-      </div>
-    </div>
-  </div> -->
+  <?php echo FrmFormsController::get_form_shortcode( array( 'id' => 2, 'title' => false, 'description' => false ) ); ?>
 
 </section>
